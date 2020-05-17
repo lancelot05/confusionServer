d64d7426b3212dbe4741d845f1cc88ed15e39198
lancelot05 <53702551+lancelot05@users.noreply.github.com>
2020-05-17 21:59:59 +0530

    mongoose population

--- routes/users.js
+++ routes/users.js
@@ -19,10 +19,28 @@ router.post("/signup", (req, res) => {
       res.setHeader("Content-type", "application/json");
       res.json({err: err})
     } else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader("Content-type", "application/json");
-        res.json({ success: true, status: "Registration Successful!", user: user });
+      if (req.body.firstname) {
+        user.firstname = req.body.firstname;
+      }
+      if (req.body.lastname) {
+        user.lastname = req.body.lastname;
+      }
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader("Content-type", "application/json");
+          res.json({ err: err });
+          return;
+        }
+        passport.authenticate("local")(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader("Content-type", "application/json");
+          res.json({
+            success: true,
+            status: "Registration Successful!",
+            user: user,
+          });
+        });
       })
     }
   })
